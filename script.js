184eb37d502d200a48f0b15a1fe2210ada970550
Wederson de Oliveira Calixto <wederson.programador@gmail.com>
2022-05-09 01:31:29 -0300

    fix: clock counting and schedule exchange events

--- script.js
+++ script.js
@@ -1,95 +1,97 @@
-var play = document.querySelector("#play");
-var pause = document.querySelector("#pause");
-var stop = document.querySelector("#stop");
-var breakTime = false;
+// Isso pode ser declarado como constantes, já que não recebem uma nova atribuição
+const playButton = document.querySelector('#play')
+const pauseButton = document.querySelector('#pause')
+const stopButton = document.querySelector('#stop')
 
-play.addEventListener("click", function () {
-    var timerInterval;
-    minutes = document.querySelector("#minutes");
-    seconds = document.querySelector("#seconds");
+// Timer
+const minutesOnTimer = document.querySelector('#minutes')
+const secondsOnTimer = document.querySelector('#seconds')
 
-    if (breakTime == false) {
-        timerInterval = setInterval(workTimer, 1000);
-    } else {
-        timerInterval = setInterval(breakTimer, 1000);
-    }
+// Divide por 60 e se tem os segundos
+const workSessionDuration = 1500 // 25min
+const breakSessionDuration = 300 // 5min
+let timeLeftOnSession = 0
+let isTimerRunning = false
+let isTimerStopped = false
+let currentSchedule = 'Work'
+let timer
 
-    if (minutes.textContent == 25 && seconds.textContent == "00") {
-        minutes.textContent = 24;
-        seconds.textContent = 59;
-    }
-    if (breakTime && seconds.textContent == "00") {
-        minutes.textContent = 4;
-        seconds.textContent = 59;
-    }
+// Atualiza dos números no relógio
+function setUpdatedClockTimers() {
+  const secondsLeft = timeLeftOnSession
+  const seconds = secondsLeft % 60
+  const minutes = parseInt(secondsLeft / 60) % 60
 
-    if (minutes.textContent == 0 && seconds.textContent == "00") {
-        timerInterval = clearInterval(timerInterval);
-    }
-    play.addEventListener("click", function () {
-        timerInterval = clearInterval(timerInterval);
-    });
-});
+  // Adiciona ZEROS a esquerda
+  const addLeadingZeroes = time => (time < 10 ? `0${time}` : time)
 
-pause.addEventListener("click", function () {
-    minutes = minutes.textContent;
-    seconds = seconds.textContent;
-});
-
-stop.addEventListener("click", function () {
-    minutes = document.querySelector("#minutes");
-    seconds = document.querySelector("#seconds");
-    if (breakTime == false) {
-        minutes.textContent = 25;
-        seconds.textContent = "00";
-    } else {
-        minutes.textContent = 5;
-        seconds.textContent = "00";
-    }
-});
-
-function workTimer() {
-    while (seconds.textContent != 0) {
-        seconds.innerHTML--;
-        if (seconds.textContent == 0) {
-            minutes.textContent--;
-            seconds.textContent = 60;
-            if (minutes.textContent < 0) {
-                minutes.textContent = 5;
-                seconds.textContent = "00";
-                breakTime = true;
-                clearInterval(timerInterval);
-
-                return seconds.textContent && minutes.textContent;
-            }
-            return seconds.textContent && minutes.textContent;
-        }
-        if (seconds.textContent <= 9) {
-            return (seconds.innerHTML = "0" + seconds.textContent);
-        }
-        return seconds.textContent;
-    }
+  minutesOnTimer.textContent = addLeadingZeroes(minutes)
+  secondsOnTimer.textContent = addLeadingZeroes(seconds)
 }
 
-function breakTimer() {
-    while (seconds.textContent != 0) {
-        seconds.textContent--;
-        if (seconds.textContent == 0) {
-            minutes.textContent--;
-            seconds.textContent = 60;
-            if (minutes.textContent < 0) {
-                minutes.textContent = 25;
-                seconds.textContent = "00";
-                breakTime = false;
-                clearInterval(timerInterval);
-
-                return seconds.textContent && minutes.textContent;
-            }
-            return seconds.textContent && minutes.textContent;
-        }
-        if (seconds.textContent <= 9) {
-            return (seconds.innerHTML = "0" + seconds.textContent);
-        }
-        return seconds.textContent;
+function countOneStepDown() {
+  if (timeLeftOnSession > 0) {
+    timeLeftOnSession--
+  } else if (timeLeftOnSession === 0) {
+    // Realiza a troca entre a PAUSA e a TAREFA
+    if (currentSchedule === 'Work') {
+      timeLeftOnSession = breakSessionDuration
+      currentSchedule = 'Break'
+    } else {
+      timeLeftOnSession = workSessionDuration
+      currentSchedule = 'Work'
     }
+  }
+  setUpdatedClockTimers()
 }
+
+// Reseta os contadores do TIMER
+function stopClockCounting() {
+  clearInterval(timer)
+  isTimerRunning = false
+  timeLeftOnSession = workSessionDuration
+  currentSchedule = 'Work'
+  setUpdatedClockTimers()
+}
+
+// INICIA, PAUSA ou RESETA o timer
+function toggleTimer(reset = false) {
+  // RESET
+  if (reset) {
+    stopClockCounting()
+    return
+  }
+
+  // Recomeça a contagem se estiver PAUSADO
+  if (isTimerStopped) {
+    setUpdatedClockTimers()
+    isTimerStopped = false
+  }
+
+  // INICIA uma contagem
+  if (isTimerRunning) {
+    clearInterval(timer)
+    isTimerRunning = false
+  } else {
+    timer = setInterval(() => {
+      countOneStepDown()
+      setUpdatedClockTimers()
+    }, 1000)
+    isTimerRunning = true
+  }
+}
+
+// Inicia ou remoceça a contagem
+playButton.addEventListener('click', function () {
+  toggleTimer()
+})
+
+// Pausa a contagem
+pauseButton.addEventListener('click', function () {
+  toggleTimer()
+})
+
+// Reseta as configurações do relógio
+stopButton.addEventListener('click', function () {
+  toggleTimer(true)
+})
